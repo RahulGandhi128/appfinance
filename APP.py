ce5549a0a7e604f7c69a92a9d39951bde5ae2043
RahulGandhi128 <159243860+RahulGandhi128@users.noreply.github.com>
2024-09-02 11:53:35 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Import necessary functions
-from Functions2.py import (
+from Functions2 import (
     function_1, function_2, function_3, function_4, function_5, function_6,
     function_7, process_company_data, scrape_table, scrape_table_with_links,
     get_quarterly_income_statements, calculate_number_of_shares, 
@@ -21,11 +21,6 @@ selected_function = st.sidebar.selectbox(
     [
         "Scrape Nifty50 Data",
         "Scrape Company Names and Links",
-        "Get Company Link",
-        "Scrape Quarterly P&L",
-        "Scrape Income Statement",
-        "Scrape Balance Sheet",
-        "Scrape Cash Flow Statement",
         "Process Company Data"
     ]
 )
@@ -41,47 +36,9 @@ elif selected_function == "Scrape Company Names and Links":
     df_links = function_2()
     st.write(df_links)
 
-elif selected_function == "Get Company Link":
-    st.header("Get Company Link")
-    company_name = st.text_input("Enter Company Name:")
-    if company_name:
-        df_links = function_2()  # You need df_links to use this function
-        company_link = function_3(company_name, df_links)
-        st.write(f"Company Link: {company_link}")
-
-elif selected_function == "Scrape Quarterly P&L":
-    st.header("Scrape Quarterly P&L")
-    company_link = st.text_input("Enter Company Link:")
-    if company_link:
-        pnl_df = function_4(company_link)
-        st.write(pnl_df)
-
-elif selected_function == "Scrape Income Statement":
-    st.header("Scrape Income Statement")
-    company_link = st.text_input("Enter Company Link:")
-    if company_link:
-        income_df = function_5(company_link)
-        st.write(income_df)
-
-elif selected_function == "Scrape Balance Sheet":
-    st.header("Scrape Balance Sheet")
-    company_link = st.text_input("Enter Company Link:")
-    if company_link:
-        balance_df = function_6(company_link)
-        st.write(balance_df)
-
-elif selected_function == "Scrape Cash Flow Statement":
-    st.header("Scrape Cash Flow Statement")
-    company_link = st.text_input("Enter Company Link:")
-    if company_link:
-        cash_flow_df = function_7(company_link)
-        st.write(cash_flow_df)
-
 elif selected_function == "Process Company Data":
     st.header("Process Company Data")
     company_name = st.text_input("Enter Company Name:")
     if company_name:
-        df_links = function_2()
-        process_company_data(company_name)
-
-# Additional functionalities or outputs can be added as per your requirement
+        df_links = function_2()  # Fetch the company links only once
+        process_company_data(company_name, df_links)
