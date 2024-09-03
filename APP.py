112b2eff8c26a9fa16b3ab39e92e21a9840b55dc
RahulGandhi128 <159243860+RahulGandhi128@users.noreply.github.com>
2024-09-03 13:53:09 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -11,7 +11,7 @@ from functions1 import (
     get_quarterly_income_statements, calculate_number_of_shares, 
     calculate_firm_metrics, calculate_adjusted_statistics, calculate_combined_metrics,
     transpose_and_clean_df, clean_and_convert_to_float, calculate_investment_rate,
-    set_api_key, configure_genai, function_1, function_2
+    set_api_key, configure_genai, function_1, function_2, get_company_link
 )
 
 # Streamlit App
